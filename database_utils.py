223d5ff94ac36cf395ffcec4d1b90a1dd5ed04e2
dario de stefano <dario.destefano@gmail.com>
2023-10-28 19:29:08 +0100

    adds a method to return sqlalchemy engine from credentials

--- database_utils.py
+++ database_utils.py
@@ -1,14 +1,38 @@
 import yaml
+from sqlalchemy import create_engine
 
 class DatabaseConnector:
 
-    def read_db_creds():
+    def __init__(self) -> None:
+        pass
+
+    def read_db_creds(self):
+
+        """This method reads from a yaml file that stores the credentials to access an AWS RDS"""
 
         with open('db_creds.yaml','r') as credentials: 
 
             cred_dict=yaml.safe_load(credentials)
             
             return cred_dict
+    
+    def init_db_engine(self):
+
+        DATABASE_TYPE = 'postgresql'
+        DBAPI = 'psycopg2'
+        ENDPOINT = self.read_db_creds()['RDS_HOST'] 
+        USER = self.read_db_creds()['RDS_USER'] 
+        PASSWORD = self.read_db_creds()['RDS_PASSWORD'] 
+        PORT = self.read_db_creds()['RDS_PORT'] 
+        DATABASE = self.read_db_creds()['RDS_DATABASE'] 
+
+
+        engine = create_engine(f"{DATABASE_TYPE}+{DBAPI}://{USER}:{PASSWORD}@{ENDPOINT}:{PORT}/{DATABASE}")
+        engine = engine.connect()
+
+        return engine
+
+
             
-
-
+ABC=DatabaseConnector()
+print(ABC.init_db_engine())
